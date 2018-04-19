8a58c946edf129f7ca77210d71008703a599d972
Digvijay Anil Gunjal <digvijay019@gmail.com>
2018-04-19 11:15:55 +0530

    dg rg - customer can create html statement

--- src/main/java/com/thoughtworks/movierental/model/Customer.java
+++ src/main/java/com/thoughtworks/movierental/model/Customer.java
@@ -3,70 +3,88 @@ package com.thoughtworks.movierental.model;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 import static java.lang.String.valueOf;
 import static java.util.stream.Collectors.joining;
 
 @Entity
 public class Customer {
-  @Id
-  @GeneratedValue
-  private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
-  @Column
-  private String name;
+    @Column
+    private String name;
 
-  @Column
-  private String email;
+    @Column
+    private String email;
 
-  @OneToMany(targetEntity = Rental.class, mappedBy = "customer")
-  private List<Rental> rentals = new ArrayList<>();
+    @OneToMany(targetEntity = Rental.class, mappedBy = "customer")
+    private List<Rental> rentals = new ArrayList<>();
 
-  protected Customer(){}
-
-  public Customer(String name, String email) {
-    this.name = name;
-    this.email = email;
-  }
-
-  public void addRental(Rental arg) {
-    arg.setCustomer(this);
-    rentals.add(arg);
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String statement() {
-      return statementHeader() + statementBody() + statementFooter();
-  }
-
-  private String statementHeader() {
-    return "Rental Record for " + getName() + "\n";
-  }
-
-  private String statementBody() {
-    return rentals.stream()
-            .map((rental) -> "\t" + rental.getMovie().getTitle() + "\t" + valueOf(rental.amount()) + "\n")
-            .collect(joining());
+    protected Customer() {
     }
 
-  private String statementFooter() {
-    return "Amount owed is " + valueOf(totalRentalAmount()) + "\n" + "You earned " + valueOf(totalRenterPoints()) + " frequent renter points";
-  }
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 
-  private double totalRentalAmount() {
-    return rentals.stream().mapToDouble(Rental::amount).sum();
-  }
+    public void addRental(Rental arg) {
+        arg.setCustomer(this);
+        rentals.add(arg);
+    }
 
-  private int totalRenterPoints() {
-    return rentals.stream().mapToInt(Rental::frequentRenterPoints).sum();
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String statement() {
+        return statementHeader() + statementBody() + statementFooter();
+    }
+
+    public String htmlStatement() {
+        return statementHeaderHtml() + statementBodyHtml() + statementFooterHtml();
+    }
+
+    private String statementHeader() {
+        return "Rental Record for " + getName() + "\n";
+    }
+
+    private String statementHeaderHtml() {
+        return "<h1>" + ("Rental Record for " + getName()) + "</h1>";
+    }
+
+    private String statementBody() {
+        return rentals.stream()
+                .map((rental) -> "\t" + rental.getMovie().getTitle() + "\t" + valueOf(rental.amount()) + "\n")
+                .collect(joining());
+    }
+
+    private String statementBodyHtml() {
+        return rentals.stream()
+                .map((rental) -> "<h3>" + rental.getMovie().getTitle() + "</h3>-&nbsp<b>" + valueOf(rental.amount()) + "</b></br>")
+                .collect(joining());
+    }
+
+    private String statementFooter() {
+        return "Amount owed is " + valueOf(totalRentalAmount()) + "\n" + "You earned " + valueOf(totalRenterPoints()) + " frequent renter points";
+    }
+
+    private String statementFooterHtml() {
+        return "<b>" + "Amount owed is " + valueOf(totalRentalAmount()) + "</b></br><b>" + "You earned " + valueOf(totalRenterPoints()) + " frequent renter points" + "</b>";
+    }
+
+    private double totalRentalAmount() {
+        return rentals.stream().mapToDouble(Rental::amount).sum();
+    }
+
+    private int totalRenterPoints() {
+        return rentals.stream().mapToInt(Rental::frequentRenterPoints).sum();
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
 
--- src/test/java/com/thoughtworks/movierental/model/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/model/CustomerTest.java
@@ -49,4 +49,31 @@ public class CustomerTest {
 
         assertEquals(expectedStatement, customer.statement());
     }
+
+    @Test
+    public void shouldCreateHTMLStatement() {
+        Rental regularRental1 = new Rental(new Movie("Rang de", REGULAR), 1);
+        Rental regularRental2 = new Rental(new Movie("Padmavat", REGULAR), 3);
+        Rental childrensRental1 = new Rental(new Movie("Despicable me", CHILDRENS), 4);
+        Rental childrensRental2 = new Rental(new Movie("Despicable me 2", CHILDRENS), 3);
+        Rental newRelease1 = new Rental(new Movie("Jagga Jasoos", NEW_RELEASE), 1);
+        Rental newRelease2 = new Rental(new Movie("Avengers", NEW_RELEASE), 2);
+        Customer customer = new Customer("Amitabh Bacchan", "amitabh@bacchan.com");
+
+        customer.addRental(regularRental1);
+        customer.addRental(regularRental2);
+        customer.addRental(childrensRental1);
+        customer.addRental(childrensRental2);
+        customer.addRental(newRelease1);
+        customer.addRental(newRelease2);
+
+        String expectedStatement = "<h1>Rental Record for Amitabh Bacchan</h1>" +
+                "<h3>Rang de</h3>-&nbsp<b>2.0</b></br><h3>Padmavat</h3>-&nbsp<b>3.5</b></br>" +
+                "<h3>Despicable me</h3>-&nbsp<b>3.0</b></br><h3>Despicable me 2</h3>-&nbsp<b>1.5</b></br>" +
+                "<h3>Jagga Jasoos</h3>-&nbsp<b>3.0</b></br><h3>Avengers</h3>-&nbsp<b>6.0</b></br>" +
+                "<b>Amount owed is 19.0</b></br>" +
+                "<b>You earned 7 frequent renter points</b>";
+
+        assertEquals(expectedStatement, customer.htmlStatement());
+    }
 }
